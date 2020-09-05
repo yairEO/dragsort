4a3a82457227672e4f916882a01d6ab97e2903db
vsync <vsync.design@gmail.com>
2020-09-05 13:56:50 +0300

    upgrading after a long time for the use in Tagify

--- README.md
+++ README.md
@@ -1,44 +1,48 @@
-[dragSort](https://codepen.io/vsync/pen/3f6b998fa1bb1b7c7f74ec89152f39f9/?editors=0100) - Animated drag sorting for horizontal list items
+<p align="center">
+  <a href='https://www.npmjs.com/package/@yaireo/dragsort'>
+      <img src="https://img.shields.io/npm/v/@yaireo/dragsort.svg" />
+  </a>
+  <a href='https://simple.wikipedia.org/wiki/MIT_License'>
+      <img src="https://img.shields.io/badge/license-MIT-lightgrey" />
+  </a>
+  <img src="https://img.shields.io/bundlephobia/minzip/@yaireo/dragsort" />
+  <img src="https://img.shields.io/npm/dw/@yaireo/dragsort" />
+</p>
+
+[dragSort](https://codepen.io/vsync/pen/3f6b998fa1bb1b7c7f74ec89152f39f9/?editors=0100) - lightweight HTML5 drag-&-drop sorting
 ========
 
-
 <a align="center" href="https://codepen.io/vsync/pen/3f6b998fa1bb1b7c7f74ec89152f39f9/?editors=0100">
-    
-![Watch the video](https://raw.githubusercontent.com/yairEO/dragsort/master/demo.gif)
+
+![drag-sort demo](https://raw.githubusercontent.com/yairEO/dragsort/master/demo.gif)
 
 </a>
 
-
-
-
-
 ## Installation
-
+```sh
     npm i @yaireo/dragsort --save
-
-    // usage:
-    import DragSort from '@yaireo/dragsort'
-
-    var dragsort = new DragSort(...)
+```
 
 ## Usage
 
-### HTML
+#### HTML
+```html
+<ul class="list">
+    <li>A</li>
+    <li>BBBBB</li>
+    <li>CCCCCCCCC</li>
+    <li>DDDD DDDDDDDD</li>
+    <li>EE</li>
+</ul>
+```
 
-    <ul class="list">
-        <li>A</li>
-        <li>BBBBB</li>
-        <li>CCCCCCCCC</li>
-        <li>DDDD DDDDDDDD</li>
-        <li>EE</li>
-    </ul>
-
-### javascript
-
+#### javascript
+```js
     var listElm = document.querySelector('.list'),
         dragSort = new DragSort(listElm);
+```
 
 ## Pre-setup suggestions:
 
-* Set `box-sizing: border-box` on your list's children is a good idea
+* Setting `box-sizing: border-box` on your list's children is a good idea
 * Should include the `dragsort.css` file (from this repository)
--- dragsort.js
+++ dragsort.js
@@ -1,219 +1,295 @@
-/*
-    dragSort
-    (c) Yair Even Or
-    https://github.com/yairEO/dragsort
-*/
+var DragSort = (function (elm) {
+    var _id = 0,
+        _current = {}, // currently-dragged element
+        _instances = {}
 
-;(function (root, factory) {
-    var define = define || {};
-    if (typeof define === 'function' && define.amd)
-        define([], factory);
-    else if (typeof exports === 'object' && typeof module === 'object')
-        module.exports = factory();
-    else if (typeof exports === 'object')
-        exports["DragSort"] = factory();
-    else
-        root.FormValidator = factory();
-}(this, function(){
-    return DragSort;
-})();
+    https://stackoverflow.com/a/14570614/104380
+    var observeDOM = (function(){
+        var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
-function DragSort(parentElm) {
-    if (!parentElm) return this;
+        return function( obj, callback ){
+            if( !obj || obj.nodeType !== 1 ) return; // validation
 
-    this.parentElm = parentElm;
-    this.setup();
+            if( MutationObserver ){
+                // define a new observer
+                var obs = new MutationObserver(function(mutations, observer){
+                    callback(mutations)
+                })
 
-    this.settings = {
-        gap: this.getItemsGap(parentElm.firstElementChild)
+                obs.observe(obj, {childList:true, subtree:false})
+            }
+
+            else if( window.addEventListener )
+                obj.addEventListener('DOMNodeInserted', callback, false)
+        }
+    })()
+
+    /////////////////////////////////////
+
+    function _DragSort(elm, settings) {
+        if (!elm) return this;
+
+        settings = settings || {}
+        this.parentElm = elm;
+        this.uid = settings.uid;
+
+        this.settings = {
+            selector: '*',
+            callbacks: {}
+        }
+
+        Object.assign(this.settings, settings)
+
+        this.setup()
+        this.bindEvents()
     }
 
-    this.bindEvents();
-}
+    _DragSort.prototype = {
+        namespace: 'dragsort',
 
-DragSort.prototype = {
-    namespace: 'dragsort',
-    setup() {
-        // enables each list item to be draggable
-        [...this.parentElm.childNodes].forEach(elm => {
-            if (elm.nodeType != 1)
-                return elm.parentNode.removeChild(elm);
-            elm.draggable = true;
-        })
-    },
+        setup() {
+            // remove non-element nodes
+            [...this.parentElm.childNodes].forEach(elm => {
+                if (elm.nodeType != 1)
+                    return elm.parentNode.removeChild(elm)
+                // set the "draggable" property on what's left
+                // https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/draggable
 
-    getDraggableElm(elm) {
-        return elm.closest('[draggable]');
-    },
+                if( elm.matches(this.settings.selector) )
+                    elm.draggable = true
+            })
 
-    afterDragStart() {
-        this.parentElm.classList.add(`${this.namespace}--dragStart`);
+            this.gap = this.getItemsGap(this.parentElm.firstElementChild)
 
-        // for hiding the source element with transition, the initial "width" style must be set
-        this.source.elm.style.width = this.source.elm.clientWidth + 'px';
+            observeDOM(this.parentElm, this.setup.bind(this))
+        },
 
-        this.source.elm.classList.add(`${this.namespace}--dragElem`);
-    },
+        throttle(cb, limit) {
+            var wait = false;
+            var that = this;
+            return function (args) {
+                if (!wait) {
+                    cb.call(that, args);
+                    wait = true;
+                    setTimeout(() => wait = false, limit)
+                }
+            }
+        },
 
-    dragstart(e, elm) {
-        var draggableElm = this.getDraggableElm(elm);
-        if (!draggableElm) return;
+        getDraggableElm(elm) {
+            var draggableElm = elm.closest('[draggable="true"]')
+            // only allow dragging/dropping inside the same parent element
+            return (this.uid == _current.uid) ? draggableElm : null
+        },
 
-        this.source = this.getInitialState();
-        this.target = this.getInitialState();
+        dragstart(e, elm) {
+            _current = this;
+            var draggableElm = this.getDraggableElm(elm);
+            if (!draggableElm) {
+                _current = {};
+                return
+            }
+
+            this.source = this.getInitialState()
+            this.target = this.getInitialState()
+
+            this.source.elm = draggableElm
+            this.source.idx = this.getNodeIndex(draggableElm)
+            this.source.size.width = draggableElm.offsetWidth //elm.getBoundingClientRect();
+            this.source.size.height = draggableElm.offsetHeight
+            // https://developer.mozilla.org/en-US/docs/Web/API/DataTransfer/effectAllowed
+            e.dataTransfer.effectAllowed = 'move'
+
+            // https://stackoverflow.com/q/19639969/104380
+            setTimeout(this.afterDragStart.bind(this))
+        },
+
+        afterDragStart() {
+            this.parentElm.classList.add(`${this.namespace}--dragStart`)
+
+            // 'יקמ' hiding the source element with transition, the initial "width" is set to occupy the same space
+            this.source.elm.style.width = this.source.elm.clientWidth + 'px'
+
+            this.source.elm.classList.add(`${this.namespace}--dragElem`)
+        },
+
+        dragover(e) {
+            e.preventDefault()
+            e.stopPropagation()
+
+            var elm = e.target;
+
+            elm = this.getDraggableElm(elm)
+
+            if (!elm || !this.target) return;
+
+            var prevTarget = {
+                elm: this.target.elm,
+                hoverDirection: this.target.hoverDirection
+            }
+
+            e.dataTransfer.dropEffect = "move";
+
+            this.target.hoverDirection = this.getTargetDirection(e);
+            // Continue only if there was a reason for a change
+            if (prevTarget.elm != elm || prevTarget.hoverDirection != this.target.hoverDirection)
+                this.directionAwareDragEnter(e, elm);
+        },
+
+        dragenter(e, elm) {
+            elm = this.getDraggableElm(elm);
+
+            if (!elm || !this.target) return;
+
+            if (!this.isValidElm(elm) || this.source.elm == elm || !this.source.elm)
+                return;
+
+            this.target.bounding = elm.getBoundingClientRect();
+        },
+
+        // only gets called once the mouse direction is knowsn (entering from left/right)
+        directionAwareDragEnter(e, elm) {
+            e.preventDefault();
+            e.stopPropagation();
+
+            var idxDelta;
+
+            e.dataTransfer.dropEffect = 'none';
+
+            if (!this.isValidElm(elm) || this.source.elm == elm || !this.source.elm)
+                return;
+
+            e.dataTransfer.dropEffect = 'move'; // See the section on the DataTransfer object.
+            this.cleanupLastTarget();
+            this.target.elm = elm;
+            this.target.idx = this.getNodeIndex(elm);
+            elm.classList.add('over');
+
+            // if target is same as the source, un-hide the source
+            idxDelta = Math.abs(this.target.idx - this.source.idx);
+
+            this.source.elm.classList.toggle(`${this.namespace}--hide`, idxDelta > 0);
+
+            //   if( this.isTargetLastChild() && this.target.hoverDirection )
+            //       return;
+
+            if (this.settings.mode == 'vertical')
+                this.target.elm.style[this.target.hoverDirection ? 'marginBottom' : 'marginTop'] = this.source.size.height + this.gap + 'px';
+            else
+                this.target.elm.style[this.target.hoverDirection ? 'marginRight' : 'marginLeft'] = this.source.size.width + this.gap + 'px';
+        },
+
+        dragend(e) {
+            clearTimeout(this.dragoverTimeout);
+            this.dragoverTimeout = null;
+            this.parentElm.classList.remove(`${this.namespace}--dragStart`);
+
+            if (!this.isValidElm(this.target.elm)) {
+                return this.cleanup();
+            }
+
+            var insertBeforeElm = this.target.hoverDirection ? this.target.elm.nextElementSibling : this.target.elm;
 
 
-        this.source.elm = draggableElm;
-        this.source.idx = this.getNodeIndex(draggableElm);
-        this.source.size.width = draggableElm.offsetWidth //elm.getBoundingClientRect();
+            if (this.source.elm != this.target.elm && this.target.elm) {
+                this.target.elm.classList.add(`${this.namespace}--noAnim`)
+                this.cleanup();
+                this.parentElm.insertBefore(this.source.elm, insertBeforeElm);
+            }
 
-        e.dataTransfer.effectAllowed = 'move';
+            this.source.elm && this.source.elm.classList.remove(`${this.namespace}--dragElem`, `${this.namespace}--hide`)
+            this.settings.callbacks.dragEnd(this.source.elm)
 
-        // https://stackoverflow.com/q/19639969/104380
-        setTimeout(this.afterDragStart.bind(this));
-    },
+            return this
+        },
 
-    dragover(e, elm) {
-        e.preventDefault();
-        e.stopPropagation();
+        /////////////////////////////
 
-        elm = this.getDraggableElm(elm);
+        isTargetLastChild() {
+            return this.parentElm.lastElementChild == this.target.elm;
+        },
 
-        var prevTarget = {
-            elm: this.target.elm,
-            hoverDirection: this.target.hoverDirection
-        };
+        getTargetDirection(e) {
+            if (!this.target.bounding) return;
+            return this.settings.mode == 'vertical' ?
+                e.pageY > (this.target.bounding.top + this.target.bounding.height / 2) ? 1 : 0 :
+                e.pageX > (this.target.bounding.left + this.target.bounding.width / 2) ? 1 : 0
+        },
 
-        // throttle
-        if (!this.dragoverTimeout)
-            this.dragoverTimeout = setTimeout(() => {
-                this.target.hoverDirection = this.getTargetDirection(e);
-                // Continue only if there was a reason for a change
-                if (prevTarget.elm != elm || prevTarget.hoverDirection != this.target.hoverDirection)
-                    this.directionAwareDragEnter(e, elm);
+        getNodeIndex(node) {
+            var index = 0;
+            while ((node = node.previousSibling))
+                if (node.nodeType != 3 || !/^\s*$/.test(node.data))
+                    index++;
+            return index;
+        },
 
-                this.dragoverTimeout = null;
-            }, 100)
-    },
+        isValidElm(elm) {
+            return elm && elm.nodeType && elm.parentNode == this.parentElm;
+        },
 
-    dragenter(e, elm) {
-        var draggableElm = this.getDraggableElm(elm);
+        cleanup() {
+            _current = {};
 
-        if (!this.isValidElm(elm) || this.source.elm == elm || !this.source.elm)
+            [...this.parentElm.children].forEach(elm => {
+                elm.removeAttribute('style')
+                setTimeout(() => {
+                    elm.classList.remove(`${this.namespace}--over`, `${this.namespace}--noAnim`, `${this.namespace}--dragElem`)
+                }, 50)
+            })
+
             return;
+        },
 
-        this.target.bounding = elm.getBoundingClientRect();
-    },
+        cleanupLastTarget() {
+            if (this.target.elm) {
+                this.target.elm.classList.remove(`${this.namespace}--hide`, `${this.namespace}--over`)
+                this.target.elm.removeAttribute('style');
+            }
+        },
 
-    // only gets called once the mouse direction is knowsn (entering from left/right)
-    directionAwareDragEnter(e, elm) {
-        e.preventDefault();
-        e.stopPropagation();
+        getInitialState() {
+            return {
+                elm: null,
+                size: {}
+            }
+        },
 
-        var idxDelta;
+        getItemsGap(elm) {
+            var styles = getComputedStyle(elm);
+            return this.settings.mode == 'vertical' ?
+                parseInt(styles.marginTop) + parseInt(styles.marginBottom) :
+                parseInt(styles.marginLeft) + parseInt(styles.marginRight)
+        },
 
-        e.dataTransfer.dropEffect = 'none';
+        bindEvents(unbind) {
+            this.listeners = this.listeners || {
+                dragstart: e => this.dragstart(e, e.target),
+                dragenter: e => this.dragenter(e, e.target),
+                dragend  : e => this.dragend(e, e.target),
+                dragover : this.throttle(this.dragover, 350),
+            }
 
-        if (!this.isValidElm(elm) || this.source.elm == elm || !this.source.elm)
-            return;
+            for (var method in this.listeners)
+                this.parentElm[unbind ? "removeEventListener" : "addEventListener"](method, this.listeners[method])
+        },
 
-        e.dataTransfer.dropEffect = 'move'; // See the section on the DataTransfer object.
-        this.cleanupLastTarget();
-        this.target.elm = elm;
-        this.target.idx = this.getNodeIndex(elm);
-        elm.classList.add('over');
-
-        // if target is same as the source, un-hide the source
-        idxDelta = Math.abs(this.target.idx - this.source.idx);
-
-        this.source.elm.classList.toggle(`${this.namespace}--hide`, idxDelta > 0);
-
-        if (this.isTargetLastChild() && this.target.hoverDirection)
-            return;
-
-        this.target.elm.style[this.target.hoverDirection ? 'marginRight' : 'marginLeft'] = this.source.size.width + this.settings.gap + 'px';
-    },
-
-    dragend(e) {
-        clearTimeout(this.dragoverTimeout);
-        this.dragoverTimeout = null;
-        this.parentElm.classList.remove(`${this.namespace}--dragStart`);
-
-        if (!this.isValidElm(this.target.elm)) {
-            return this.cleanup();
+        destroy() {
+            this.cleanup()
+            this.bindEvents(true)
+            delete _instances[this.uid]
         }
-
-        var insertBeforeElm = this.target.hoverDirection ? this.target.elm.nextElementSibling : this.target.elm;
-
-        if (this.source.elm != this.target.elm && this.target.elm) {
-            this.target.elm.classList.add(`${this.namespace}--noAnim`)
-            this.cleanup();
-            this.parentElm.insertBefore(this.source.elm, insertBeforeElm);
-        }
-
-        this.source.elm && this.source.elm.classList.remove(`${this.namespace}--dragElem`, `${this.namespace}--hide`)
-
-        return false;
-    },
-
-    /////////////////////////////
-
-    isTargetLastChild() {
-        return this.parentElm.lastElementChild == this.target.elm;
-    },
-
-    getTargetDirection(e) {
-        if (!this.target.bounding) return;
-        return e.pageX > (this.target.bounding.left + this.target.bounding.width / 2) ? 1 : 0;
-    },
-
-    getNodeIndex(node) {
-        var index = 0;
-        while ((node = node.previousSibling))
-            if (node.nodeType != 3 || !/^\s*$/.test(node.data))
-                index++;
-        return index;
-    },
-
-    isValidElm(elm) {
-        return elm && elm.nodeType && elm.parentNode == this.parentElm;
-    },
-
-    cleanup() {
-        [...this.parentElm.children].forEach(elm => {
-            elm.removeAttribute('style')
-            setTimeout(() => {
-                elm.classList.remove(`${this.namespace}--over`, `${this.namespace}--noAnim`, `${this.namespace}--dragElem`)
-            }, 50)
-        })
-
-        return;
-    },
-
-    cleanupLastTarget() {
-        if (this.target.elm) {
-            this.target.elm.classList.remove(`${this.namespace}--hide`, `${this.namespace}--over`)
-            this.target.elm.removeAttribute('style');
-        }
-    },
-
-    getInitialState() {
-        return {
-            elm: null,
-            size: {}
-        }
-    },
-
-    getItemsGap(elm) {
-        var styles = getComputedStyle(elm);
-        return parseInt(styles.marginLeft) + parseInt(styles.marginRight);
-    },
-
-    bindEvents() {
-        var names = ['dragstart', 'dragover', 'dragenter', 'dragend'];
-
-        names.forEach(name =>
-            this.parentElm.addEventListener(name, e => this[name](e, e.target))
-        )
     }
-}
+
+    /////////////////////////////////////
+    // Factory
+    return function (elm, settings) {
+        // if this "elm" has already been initialized with DragSort, return last DragSort instance and do not create a new one
+        _instances[++_id] = elm["_DragSort"]
+            ? _instances[elm["_DragSort"]]
+            : new _DragSort(elm, {...settings, uid: _id})
+
+        elm["_DragSort"] = _id
+        return _instances[_id]
+    }
+})()
